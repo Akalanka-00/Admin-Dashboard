500c2075a6f99c56a2a0eee5923ebe50f540505f
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-01-23 14:32:13 +0530

    admin-dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import Home from './Pages/HomePage/Home'
+import About from './Pages/About/About'
+import  Settings from './Pages/Settings/Settings';
 
 function App() {
   return (
     <div className="App">
-    
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' exact element = {<Home/>}></Route>
+      <Route path='/about' exact element = {<About/>}></Route>
+      <Route path='/settings' exact element = {<Settings/>}></Route>
+
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/HomePage/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SideNav from '../../Components/SideNav/SideNav'
+const Home = () => {
+  return (
+    <div>
+      <SideNav/>
+    </div>
+  )
+}
+
+export default Home
